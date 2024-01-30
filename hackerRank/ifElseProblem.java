f5180d06a3ee49a46ed8b5e62fa1e7ac717fce9d
adityasxna <adityasaxena0022@gmail.com>
2024-01-30 15:48:42 +0530

    If Else Problem HR

--- hackerRank/ifElseProblem.java
+++ hackerRank/ifElseProblem.java
@@ -1,5 +1,22 @@
+import java.util.*;
+
 public class ifElseProblem {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n % 2 == 0) {
+            if (n >= 2 && 5 >= n){
+                System.out.println("Not Weird");
+            }
+            else if (n >= 6 && 20 >= n) {
+                System.out.println("Weird");
+            }
+            else {
+                System.out.println("Not Weird");
+            }
+        }
+        else {
+            System.out.println("Weird");
+        }
     }
 }

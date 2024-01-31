001a5f474d6c89bc9c1289d2aec1b3cd59cb61c1
adityasxna <adityasaxena0022@gmail.com>
2024-02-01 04:02:19 +0530

    changed

---
+++ hackerRank/Prob7.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Prob7 {
+    public static void main(String[] args){
+        String rev = "";
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+        if (str.equals(rev)) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+        sc.close();
+    }
+}
